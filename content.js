ca643ef460f463bb4f9dc4496103bec21040dca0
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-22 17:00:45 +0300

    123

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 window.setTimeout(function() {
     const date = new Date()
 const sicneDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-console.log("andrey idi naxyi");
 const year = sicneDate.getFullYear();
 const month = sicneDate.getMonth();
 const day = new Date().getDate();
