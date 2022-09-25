a1c271f5f95fb807cb7c3c6b5177cd9d263f1d22
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-25 16:12:26 +0300

    ref

--- content.js
+++ content.js
@@ -11,7 +11,7 @@ window.setInterval(function () {
 				return childNodes.map(i => {
 					if (i.childNodes[0].childNodes[1].childNodes[0].dataset.svgsPath ===
 						"sm1/notification_completed.svg") {
-						return getItemName(i.innerText);
+						return getItemScore(i.innerText) ?? 0;
 					} else {
 						return 0;
 					}
@@ -21,41 +21,44 @@ window.setInterval(function () {
 			})
 		}
 
-		const getItemName = name => {
+		const getItemScore = name => {
 			const scoreText = name.replaceAll("\n", " ").match(/^.+\[(?<score>\d+)\]\s*.*$/)?.groups?.["score"];
-			return scoreText ? parseInt(scoreText) : 0;
+			return scoreText ? parseInt(scoreText) : undefined;
 
 		}
 
 		const setSum = (points, numForId, parent) => {
-			if (!(parent[numForId].childNodes[0].id === "counter")) {
-				const div1 = document.createElement("div")
-				div1.innerHTML = "Total Score For This Day: " + points;
-				div1.id = `counter`
-				div1.style.fontSize = "24px"
-				div1.style.fontWeight = 600
-				parent[numForId].childNodes[0].before(div1)
+			if (parent[numForId].childNodes[0].id === "counter") {
+				return;
 			}
+
+			const div1 = document.createElement("div")
+			div1.innerHTML = "Total Score For This Day: " + points;
+			div1.id = `counter`
+			div1.style.fontSize = "24px"
+			div1.style.fontWeight = 600
+			parent[numForId].childNodes[0].before(div1)
 		}
+
 		getItemsScores(itemsArray).map((item, i = 0) => {
 			return setSum(item, i, parent);
 		})
 		function getIcons() {
 			const icons = document.getElementsByClassName("avatar_event_icon");
 			Array.from(icons).forEach((element) => {
-				if (
-					element.children[0].dataset.svgsPath ===
-					"sm1/notification_completed.svg"
-				) {
-					let text =
-						element.parentElement.parentElement.childNodes[1].childNodes[0]
-							.childNodes[2].childNodes[0].childNodes[0].textContent;
-					const regexp = /\[\d*\]/;
+				if (element.children[0].dataset.svgsPath !== "sm1/notification_completed.svg") {
+					return;
+				}
 
-					if (!text.match(regexp)) {
-						element.parentElement.parentElement.childNodes[1].childNodes[0].childNodes[2].style.backgroundColor =
-							"red";
-					}
+				const text =
+					element.parentElement.parentElement.childNodes[1].childNodes[0]
+						.childNodes[2].childNodes[0].childNodes[0].textContent;
+						
+				const score = getItemScore(text);
+
+				if (!score) {
+					element.parentElement.parentElement.childNodes[1].childNodes[0].childNodes[2].style.backgroundColor =
+						"red";
 				}
 			});
 		}
@@ -75,10 +78,6 @@ window.setInterval(function () {
 				const dataSet = element.dataset.itemId;
 				const url = "https://todoist.com/app/task/" + dataSet + "/0"
 				element.childNodes[0].childNodes[4].before(button)
-				if (element.childNodes[0].childNodes[4]) {
-
-
-				}
 				button.addEventListener("click", () => {
 					window.open(url, '_blank').focus();
 				})
