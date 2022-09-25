4c366bbc571d4a34667877808fca580d2c9888e5
ntorbinskiy <n.torbinskiy@gmail.com>
2022-09-25 12:32:43 +0300

    123

--- content.js
+++ content.js
@@ -1,61 +1,82 @@
-window.setTimeout(function () {
-	const date = new Date();
-	const sicneDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-	const year = sicneDate.getFullYear();
-	const month = sicneDate.getMonth();
-	const day = new Date().getDate();
-	const newDay = day + 1;
-	const since = `${year}-${month + 1}-${day}T00:00:00`;
-	const until = `${month === 11 && day === 31 ? year + 1 : year}-${day === sicneDate.getDate() ? month + 2 : month + 1}-${newDay}T00:00:00`;
-	const url = "https://api.todoist.com/sync/v9/completed/get_all";
+window.setInterval(function () {
+    if (window.location.href.includes("https://todoist.com/app/activity")) {
 
-	const request = async (
-		url,
-		method = "GET",
-		body = null,
-		headers = {
-			"Content-Type": "application/json",
-		}
-	) => {
-		try {
-			const response = await fetch(url, {
-				method,
-				body,
-				headers,
-			});
+    const parent = document.getElementsByClassName("section")
+    const items = document.querySelectorAll("ul.items");
+    const itemsArray = Array.from(items);
+    // const itemsArray = Array.from(elem)
+	const getItemsScores = items => {
+		return items.map(item => {
+			const childNodes = [...item.childNodes];
+			return childNodes.map(i => {
+                if(i.childNodes[0].childNodes[1].childNodes[0].dataset.svgsPath ===
+                    "sm1/notification_completed.svg") {
+				        return getItemName(i.innerText);
+                    } else {
+                        return 0;
+                    }
+                    
+			}).reduce((v, k) => v + k, 0);
 
-			if (!response.ok) {
-				throw new Error(`Could not fetch ${url}, status: ${response.status}`);
-			}
-			const data = await response.json();
-
-			return data;
-		} catch (e) {
-			throw e;
-		}
-	};
-
-	function getScoreFromTaskName(name) {
-		const scoreText = name.replaceAll("\n", " ").match(/^.+\[(?<score>\d+)\]\s*$/)?.groups?.["score"];
-		return scoreText ? parseInt(scoreText) : 0;
+		})
 	}
 
-	function getTaskScores(api) {
-		return request(`${url}?since=${since}&until=${until}`, "GET", null, {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${api} `,
-		}).then((data) => data.items.reduce((sum, { content }) => getScoreFromTaskName(content) + sum, 0));
+	const getItemName = name => {
+		const scoreText = name.replaceAll("\n", " ").match(/^.+\[(?<score>\d+)\]\s*.*$/)?.groups?.["score"];
+        return scoreText ? parseInt(scoreText) : 0;
+        
 	}
 
-	getTaskScores("759a2c20d5745406b76f0f861330196f60900777").then((data) => setSum(data));
-
-	const setSum = (d) => {
-		const elem = document.getElementById("activity_app");
-		const div1 = document.createElement("div");
-		div1.innerHTML = "Total Score For This Day: " + d;
-		div1.className = "counter";
-		div1.style.fontSize = "24px";
-		div1.style.fontWeight = 600;
-		elem.before(div1);
-	};
-}, 3000);
+	const setSum = (points, numForId, parent) => {
+        if (!(parent[numForId].childNodes[0].id === "counter")) {
+            const div1 = document.createElement("div")
+            div1.innerHTML = "Total Score For This Day: " + points;
+            div1.id = `counter`
+            div1.style.fontSize = "24px"
+            div1.style.fontWeight = 600
+            parent[numForId].childNodes[0].before(div1)
+        }
+	}
+	getItemsScores(itemsArray).map((item, i = 0) => {
+		return setSum(item, i, parent);
+	})
+        function getIcons() {
+            const icons = document.getElementsByClassName("avatar_event_icon");
+            Array.from(icons).forEach((element) => {
+            if (
+                element.children[0].dataset.svgsPath ===
+                "sm1/notification_completed.svg"
+            ) {
+                let text =
+                element.parentElement.parentElement.childNodes[1].childNodes[0]
+                    .childNodes[2].childNodes[0].childNodes[0].textContent;
+                const regexp = /\[\d*\]/;
+        
+                if (!text.match(regexp)) {
+                element.parentElement.parentElement.childNodes[1].childNodes[0].childNodes[2].style.backgroundColor =
+                    "red";
+                }
+            }
+            });
+        }
+        getIcons();
+    } else if (window.location.href.includes("https://todoist.com/app/project")) {
+        if (document.getElementsByClassName("button-href").length < 1 ) {
+            const list = document.getElementsByClassName("items")
+            Array.from(list[0].childNodes).forEach(element => {
+                const button = document.createElement("a");
+                button.innerHTML = "Link"
+                button.className = "button-href";
+                button.style.fontSize = "18px";
+                button.style.display = "absolute"
+                button.style.marginRight = "250px"
+                if (element.childNodes[0].childNodes[4]) {
+                    const dataSet = element.dataset.itemId;
+                    button.href = "https://todoist.com/app/task/" + dataSet + "/0"
+                    element.childNodes[0].childNodes[4].before(button)
+                    
+                }
+            });
+        }
+    }
+}, 1000)
